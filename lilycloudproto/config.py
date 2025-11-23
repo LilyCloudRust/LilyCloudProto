9e5d4e954fb1ac6333c470e9a7f4c6dae86175f0
apple-37 <1833346463@qq.com>
2025-11-23 23:22:39 +0800

    fix:lost define type

--- lilycloudproto/config.py
+++ lilycloudproto/config.py
@@ -10,9 +10,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "26f1d8bca14a1a6e94a545f2d2efadfbfc09d859d510dc1014d78744f58b8b22"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    ARGON2_TIME_COST = int(os.getenv("ARGON2_TIME_COST", "2"))
-    ARGON2_MEMORY_COST = int(os.getenv("ARGON2_MEMORY_COST", "131072"))  # 128 MiB
-    ARGON2_PARALLELISM = int(os.getenv("ARGON2_PARALLELISM", "2"))
+    ARGON2_TIME_COST: int = int(os.getenv("ARGON2_TIME_COST", "2"))
+    ARGON2_MEMORY_COST: int = int(os.getenv("ARGON2_MEMORY_COST", "131072"))  # 128 MiB
+    ARGON2_PARALLELISM: int = int(os.getenv("ARGON2_PARALLELISM", "2"))
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"), env_file_encoding="utf-8", extra="ignore"
     )
